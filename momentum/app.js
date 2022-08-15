b5317cca284809def4d433fafd7ff2b4c207493c
wjdgh4058 <ljhmoa@gmail.com>
2022-08-15 23:06:19 +0900

    8/15

--- momentum/app.js
+++ momentum/app.js
@@ -1,5 +1,19 @@
-function name(n){
-    console.log(n+" hello");
+let age = parseInt(prompt("How old are you?"));
+
+
+if(isNaN(age || age <0)){
+    console.log('Please write a real positive number')
+}
+else if(age < 18){
+    console.log(`your age is ${age}`);
+    console.log('you are too young.')
+} else if(age >= 18 && age <= 50){
+    console.log(`your age is ${age}`);
+    console.log('you can drick!');
+}
+else{
+    console.log(`your age is ${age}`);
+    console.log('Thank you for writing your age')
+    console.log("you can't drick!");
 }
 
-name("jungho");
