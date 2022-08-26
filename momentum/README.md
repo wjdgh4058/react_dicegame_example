a5c1c912257802c9143f686ad1a4726eaf19af6f
wjdgh4058 <ljhmoa@gmail.com>
2022-08-26 17:04:13 +0900

    8/26

--- momentum/README.md
+++ momentum/README.md
@@ -5,4 +5,12 @@ step 3 . 그 event에 반응
 style 작업에 유리한 것은 css
 event, animation 에 적합한 것은 javascript
 
-classList.toggle -> 클래스가 있으면 제거 없으면 추가 해준다. 그래서 class의 포함여부를 바꿔준다.
+classList.toggle("") -> 클래스가 있으면 제거 없으면 추가 해준다. 그래서 class의 포함여부를 바꿔준다.
+
+preventDefault() -> 브라우저가 기본적으로 하는 행동을 막아준다.
+
+localStorage.setItem("key","value") -> localStorage에 값을 저장한다.
+
+localStorage.getItem() -> localStorage에 값을 가져온다.
+
+localStorage.removeItem() -> localStorage에 값을 저장한다.
--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,33 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CALSSNAME = "hidden";
+
+const USERNAME_KEY = "username";
 
 //username 검사 -> username이 없으면 안되고, 너무 긴것또한 안된다.
-function loginBtnClick(){
-    const value = loginInput.value;
-    if(value === ""){
-        console.log("please write your name");
-    }
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CALSSNAME);
+    const username = loginInput.value;
+
+    localStorage.setItem(USERNAME_KEY,username);
+
+    paintGreetings();
 }
 
-loginButton.addEventListener("click",loginBtnClick );
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CALSSNAME);
+}
+
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername === null){
+    loginForm.classList.remove(HIDDEN_CALSSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+    paintGreetings();
+}
