3bee76d260d59a92fc607d4a18b5309df76ee012
wjdgh4058 <ljhmoa@gmail.com>
2022-09-23 17:39:46 +0900

    css ex

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Wellcome back!!!!</h1>
+      <h1 className={styles.title}>Wellcome back!!!!</h1>
+      <Button text = {"continue"}/>
     </div>
   );
 }
