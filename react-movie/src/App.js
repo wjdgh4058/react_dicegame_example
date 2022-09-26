fb525e1a6a35dde434193595ec193322a86930de
wjdgh4058 <ljhmoa@gmail.com>
2022-09-26 16:56:25 +0900

    todo

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -2,23 +2,37 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(()=>{
-    console.log("created :)");
-    return () => console.log("destroyed :(")
-  }, []);
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev)
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo ===""){
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
   }
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>My To Do ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder = "Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index)=> 
+        <li key = {index}>{item}</li>)
+        }
+      </ul>
     </div>
   )
 }
---
+++ react-movie/src/cleanup.js
@@ -0,0 +1,26 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(()=>{
+    console.log("created :)");
+    return () => console.log("destroyed :(")
+  }, []);
+  return <h1>Hello</h1>
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev)
+  }
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+    </div>
+  )
+}
+
+export default App;
